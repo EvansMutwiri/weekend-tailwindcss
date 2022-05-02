e619a5d7df31e9aa1a2263f0972719341ab9f6dc
EvansMutwiri <mutwirievansm@gmail.com>
2022-05-02 14:17:44 +0300

    hero section structure and styling

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'green': '#5AE4A7',
+      'black': '#000',
+      'grey': '#E5E5E5',
+      'orange': '#F4670B',
+      'light-grey': '#F2F2F2',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
